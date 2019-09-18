914b64cac55ee0a18b8bc579212a0c266f140478
divya-prakash2 <divya.prakash2@hpe.com>
2019-09-18 12:37:52 +0530

    Fixing tabs spaces

--- src/calculator.py
+++ src/calculator.py
@@ -2,16 +2,20 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
 
+
 # Function to multiply two numbers 
 def multiply(num1, num2): 
     return num1 * num2 
-  
+
+
 # Function to divide two numbers 
 def divide(num1, num2): 
     return num1 / num2 
--- src/test_calculator.py
+++ src/test_calculator.py
@@ -13,8 +13,8 @@ class TestCalculator:
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
-	def test_multiply(self):
-	    assert 4 == calculator.multiply(2, 2)
+    def test_multiply(self):
+        assert 4 == calculator.multiply(2, 2)
 
     def test_division(self):
         assert 2 == calculator.divide(4, 2)
