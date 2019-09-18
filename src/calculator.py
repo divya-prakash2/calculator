4a2be45c0f093bcaa2799d1f5fbdf21fd65db153
Prakash <divya.prakash2@hpe.com>
2019-09-18 10:56:29 +0530

    Addition of mult and divide

--- src/calculator.py
+++ src/calculator.py
@@ -7,3 +7,11 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+    return num1 * num2 
+  
+# Function to divide two numbers 
+def divide(num1, num2): 
+    return num1 / num2 
